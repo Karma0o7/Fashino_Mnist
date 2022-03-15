1bdcf3f5b4ebbc1bf38e325dd0afa335f88e98cd
Amit Anand <83200957+Karma0o7@users.noreply.github.com>
2022-03-15 23:30:52 +0530

    Add files via upload

---
+++ detection.py
@@ -0,0 +1,24 @@
+from pickletools import optimize
+import numpy
+import matplotlib.pyplot as plt
+from sqlalchemy import false
+import tensorflow as tf
+from tensorflow import keras
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.grid(false)
+# plt.show()
+train_images = train_images/255.0
+test_images = test_images/255.0
+model = keras.Sequential([keras.layers.Flatten(input_shape=(28,28)), keras.layers.Dense(128, activation=tf.nn.relu), keras.layers.Dense(10, activation=tf.nn.softmax)])
+model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+model.fit(train_images, train_labels, epochs=5)
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print(test_acc)
+predictions = model.predict(test_images)
+print(numpy.argmax(predictions[10]))
+print(test_labels[10])
